d8761e2f9303423b98a755f464de6e7bce303ca3
TomaszGacek <gacektomasz02@gmail.com>
2025-03-19 17:07:08 +0100

    creating graph

--- src/features/aoa/Aoa.tsx
+++ src/features/aoa/Aoa.tsx
@@ -1,8 +1,9 @@
 import {Button, Group, NumberInput, TextInput} from "@mantine/core";
 import {useForm} from "@mantine/form";
-import {AoAstyles} from "./styles/AoAStyle..ts";
-import { DataSet, Network } from 'vis-network/standalone/esm/vis-network';
-import {useEffect, useRef} from "react";
+import "./styles/AoAStyle.css";
+import {Network} from 'vis-network/standalone/esm/vis-network';
+import {useEffect, useRef, useState} from "react";
+
 type Node = {
     id: number;
     label: string;
@@ -12,95 +13,128 @@ type Edge = {
     id: number;
     from: number;
     to: number;
+    label: string
 };
+type formData = {
+    nazwaCzynnosci: string,
+    czasTrwania: number,
+    poprzednie: number,
+    nastpene: number
+}
 export const Aoa = () => {
+    const [nodes, setNodes] = useState<Node[]>([]);
+    const [edges, setEdges] = useState<Edge[]>([]);
+    const networkRef = useRef<HTMLDivElement>(null);
+    const networkInstance = useRef<Network | null>(null);
     const form = useForm({
         mode: 'uncontrolled',
         initialValues: {
             nazwaCzynnosci: "A",
-            czasTrwania: '',
-            poprzednie:'',
-            nastpene:''
+            czasTrwania: 1,
+            poprzednie: 1,
+            nastpene: 2
 
         },
     });
-    const networkRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if (networkRef.current) {
-            const nodes: DataSet<Node> = new DataSet([
-                { id: 1, label: 'Node 1' },
-                { id: 2, label: 'Node 2' },
-                { id: 3, label: 'Node 3' },
-                { id: 4, label: 'Node 4' }
-            ]);
-
-            const edges: DataSet<Edge> = new DataSet([
-                {id:0, from: 1, to: 4,label:"a  2" },
-                { id:2,from: 2, to: 4 },
-                { id:3,from: 3, to: 4 }
-            ]);
-
-            const options = {};
-
-            new Network(networkRef.current, { nodes, edges }, options);
+            networkInstance.current = new Network(
+                networkRef.current,
+                {nodes: nodes, edges: edges},
+                {edges:{arrows:'to'}}
+            );
         }
     }, []);
+
+    useEffect(() => {
+        if (networkInstance.current) {
+            networkInstance.current.setData({nodes, edges});
+        }
+    }, [nodes, edges]);
+    const handleForm = (data: formData) => {
+         console.log(data)
+
+        if(!nodes.some(item => item.id === data.poprzednie)) {
+            const newNode: Node = {
+                id: data.poprzednie,
+                label: data.poprzednie.toString()
+            };
+            setNodes((prevNodes) => [...prevNodes, newNode]);
+        }
+        if(!nodes.some(item => item.id === data.nastpene)) {
+            const newNode: Node = {
+                id: data.nastpene,
+                label: data.nastpene.toString()
+            };
+            setNodes((prevNodes) => [...prevNodes, newNode]);
+        }
+
+        const newEdge: Edge = {
+            id: edges.length + 1,
+            from: data.poprzednie,
+            to: data.nastpene,
+            label: data.nazwaCzynnosci + "  " + data.czasTrwania
+        }
+
+        setEdges((prevEdges) => [...prevEdges, newEdge]);
+
+        form.reset()
+    }
     return (
-        <div style={AoAstyles.container}>
+        <div className="container">
 
-            <div style={AoAstyles.leftSide}>
+            <div className="leftSide">
 
-            <div style={AoAstyles.form}>
-                <form onSubmit={form.onSubmit((values) => {
-                    console.log(values)
-                    form.reset()
-                })}>
-                    <TextInput
+                <div className="form">
+                    <form onSubmit={form.onSubmit((values) => {
+                        handleForm(values);
+                    })}>
+                        <TextInput
 
-                        label="Nazwa czynnosci"
-                        placeholder="A"
-                        key={form.key('nazwaCzynnosci')}
-                        {...form.getInputProps('nazwaCzynnosci')}
-                    />
-                    <NumberInput
-                        label="Czas trwania czynności"
-                        description="Czas trwania czynności"
-                        placeholder="1"
-                        min={1}
-                        key={form.key('czasTrwania')}
-                        {...form.getInputProps('czasTrwania')}
-                    />
-                    <NumberInput
-                        label="Poprzednie zdarzenie"
-                        description="Poprzednie zdarzenie"
-                        placeholder="1"
-                        min={1}
-                        key={form.key('poprzednie')}
-                        {...form.getInputProps('poprzednie')}
-                    />
-                    <NumberInput
-                        label="Nastepne zdarzenie"
-                        description="Nastepne zdarzenie"
-                        placeholder="1"
-                        min={1}
-                        key={form.key('nastpene')}
-                        {...form.getInputProps('nastpene')}
-                    />
+                            label="Nazwa czynnosci"
+                            placeholder="A"
+                            key={form.key('nazwaCzynnosci')}
+                            {...form.getInputProps('nazwaCzynnosci')}
+                        />
+                        <NumberInput
+                            label="Czas trwania czynności"
+                            //   description="Czas trwania czynności"
+                            placeholder="1"
+                            min={1}
+                            key={form.key('czasTrwania')}
+                            {...form.getInputProps('czasTrwania')}
+                        />
+                        <NumberInput
+                            label="Poprzednie zdarzenie"
+                            //  description="Poprzednie zdarzenie"
+                            placeholder="1"
+                            min={1}
+                            key={form.key('poprzednie')}
+                            {...form.getInputProps('poprzednie')}
+                        />
+                        <NumberInput
+                            label="Nastepne zdarzenie"
+                            //    description="Nastepne zdarzenie"
+                            placeholder="1"
+                            min={1}
+                            key={form.key('nastpene')}
+                            {...form.getInputProps('nastpene')}
+                        />
 
 
-                    <Group justify="flex-end" mt="md">
-                        <Button type="submit">Submit</Button>
-                    </Group>
-                </form>
+                        <Group justify="flex-end" mt="md">
+                            <Button type="submit">Submit</Button>
+                        </Group>
+                    </form>
+                </div>
+                <div className="table">
+
+                </div>
+
             </div>
-            <div style={AoAstyles.table}>
 
-            </div >
-
-            </div>
-
-            <div id={'graph'} style={AoAstyles.graph}>
+            <div id={'graph'} className="graph">
                 <div ref={networkRef} style={{height: '500px'}}/>
             </div>
         </div>
